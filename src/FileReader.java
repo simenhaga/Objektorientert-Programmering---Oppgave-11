94624228a35ffbbf24a681b94ebf0006b65443be
simenhaga <simen.haga@lyse.net>
2021-04-27 15:37:02 +0200

    Did som changes in FileReader

--- src/FileReader.java
+++ src/FileReader.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class FileReader {
@@ -10,58 +11,62 @@ public class FileReader {
 
         try(Scanner scanner = new Scanner(new File(filePath))) {
             while (scanner.hasNextLine()) {
-                Shape shape;
+                Shape shape = null;
                 if (scanner.hasNext("Circle")) {
+                    scanner.nextLine();
                     //Construct circle
                     float radius = Float.parseFloat(scanner.nextLine());
-                    int r = Integer.parseInt(scanner.nextLine());
-                    int g = Integer.parseInt(scanner.nextLine());
-                    int b = Integer.parseInt(scanner.nextLine());
+                    Color color = parseColor(scanner);
                     boolean isFilled = Boolean.parseBoolean(scanner.nextLine());
-                    Point center = new Point(Float.parseFloat(scanner.nextLine()),
-                            Float.parseFloat(scanner.nextLine()));
+                    Point center = new Point(Float.parseFloat(scanner.nextLine()), Float.parseFloat(scanner.nextLine()));
 
-                    shape = new Circle(new Color(r, g, b), isFilled, radius, center);
-                    shapes.add(shape);
+                    shape = new Circle(color, isFilled, radius, center);
 
                 } else if (scanner.hasNext("Rectangle")) {
+                    scanner.nextLine();
                     //Construct rectangle
                     float height = Float.parseFloat(scanner.nextLine());
                     float width = Float.parseFloat(scanner.nextLine());
-                    int r = Integer.parseInt(scanner.nextLine());
-                    int g = Integer.parseInt(scanner.nextLine());
-                    int b = Integer.parseInt(scanner.nextLine());
+                    Color color = parseColor(scanner);
                     boolean isFilled = Boolean.parseBoolean(scanner.nextLine());
                     Point topLeft = new Point(Float.parseFloat(scanner.nextLine()),
                             Float.parseFloat(scanner.nextLine()));
                     Point bottomRight = new Point(Float.parseFloat(scanner.nextLine()),
                             Float.parseFloat(scanner.nextLine()));
 
-                    shape = new Rectangle(new Color(r, g, b), isFilled, height, width, topLeft, bottomRight);
-                    shapes.add(shape);
+                    shape = new Rectangle(color, isFilled, height, width, topLeft, bottomRight);
 
                 } else if (scanner.hasNext("Square")) {
+                    scanner.nextLine();
+
                     //Construct square
                     float side = Float.parseFloat(scanner.nextLine());
-                    int r = Integer.parseInt(scanner.nextLine());
-                    int g = Integer.parseInt(scanner.nextLine());
-                    int b = Integer.parseInt(scanner.nextLine());
+                    Color color = parseColor(scanner);
                     boolean isFilled = Boolean.parseBoolean(scanner.nextLine());
                     Point topLeft = new Point(Float.parseFloat(scanner.nextLine()),
                             Float.parseFloat(scanner.nextLine()));
                     Point bottomRight = new Point(Float.parseFloat(scanner.nextLine()),
                             Float.parseFloat(scanner.nextLine()));
 
-                    shape = new Square(new Color(r, g, b), isFilled, side, topLeft, bottomRight);
-                    shapes.add(shape);
+                    shape = new Square(color, isFilled, side, topLeft, bottomRight);
 
                 }else {
                     scanner.nextLine();
+                }
+                if (shape != null){
+                    shapes.add(shape);
                 }
             }
         }catch (FileNotFoundException e){
             System.out.println("File not found");
         }
         return shapes;
+    }
+
+    private static Color parseColor(Scanner scanner){
+        int r = Integer.parseInt(scanner.nextLine());
+        int g = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        return new Color(r, g, b);
     }
 }
