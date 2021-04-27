3e6abcb80f43f5f3eb2f0fc3f17b05d5b36d6575
simenhaga <simen.haga@lyse.net>
2021-04-27 16:09:24 +0200

    Implemented ShapeStorage and added som new thing to Application

--- src/Application.java
+++ src/Application.java
@@ -1,13 +1,43 @@
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Application {
+    static ShapeStorage storage;
 
     public static void main(String[] args) {
         //testShapes();
-        testFileReader();
+        String filepath = "Shapes.txt";
+        storage =  new ShapeStorage(FileReader.getShapesFromFile(filepath));
+        runApplication();
     }
 
+    private static void runApplication(){
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        do {
+            System.out.println("1: Draw all shapes \n2: Show combined area of squares \n3: Quit");
+            if (scanner.hasNext("1")){
+                scanner.next();
+                storage.drawAllShapes();
+            }
+            else if (scanner.hasNext("2")){
+                scanner.next();
+                storage.getSumAreaOfSquares();
+            }
+            else if (scanner.hasNext("3")) {
+                scanner.next();
+                quit();
+                quit = true;
+            }
+        } while (!quit);
+    }
+
+    private static void quit() {
+        System.out.println("Goodbye!");
+    }
+
+    // Test helper methods
     private static void testShapes() {
         Circle circle = new Circle(Color.GREEN, true, 13f, new Point(12, 3));
         Rectangle rectangle = new Rectangle(Color.BLUE, false, 3, 4, new Point(33, 44));
--- src/ShapeStorage.java
+++ src/ShapeStorage.java
@@ -1,3 +1,27 @@
-public class ShapeStorage {
+import java.util.ArrayList;
 
+public class ShapeStorage {
+    private ArrayList<Shape> shapes;
+
+    public ShapeStorage(ArrayList<Shape> shapes) {
+        this.shapes = shapes;
+    }
+
+    public void drawAllShapes(){
+        for (Shape shape : shapes){
+            shape.draw();
+        }
+    }
+    public void getSumAreaOfSquares() {
+        float totalArea = 0;
+        for (Shape shape : shapes) {
+            if (shape.getClass() == Square.class) {
+                Square square = (Square) shape;
+                float area = square.height * square.width;
+                totalArea += area;
+            }
+        }
+        String result = String.format("Area of all squares is: %.2f", totalArea);
+        System.out.println(result);
+    }
 }
